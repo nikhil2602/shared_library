d3a276583fa1e71ab38372f8f0ee04691794b8fc
nikhil2602 <65943793+nikhil2602@users.noreply.github.com>
2020-06-12 14:49:46 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,4 +1,6 @@
-mscmsal,
-  sdfk,oapdsv
-dslxmlsdve
-fsdklxmvls
+def call(Map stageParams) {
+checkout([
+  $class: 'GitSCM', 
+  branches: [[name: '*/master']],
+  userRemoteConfigs: [[url: 'https://github.com/nikhil2602/shared_library.git']]])
+}
