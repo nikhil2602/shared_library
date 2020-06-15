e3aa921a0b37f31ed7a2daad9b501342cb418c72
nikhil2602 <65943793+nikhil2602@users.noreply.github.com>
2020-06-15 12:40:38 +0530

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,4 +1,5 @@
-a
-b
-c
-d
+def call(String jobName,String buildNumber,StringbuildStatus) {
+  //echo "Hello, ${name}."
+  //slackSend (color: '#FFFF00', message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' SUCCESS")
+  slackSend (color: '#FFFF00', message: "STARTED: Job 'jobName [buildNumber]' buildStatus")
+}
