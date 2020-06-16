bbb3d0bdcdeff9fe2d9c03ff75cc0197f970d38b
nikhil2602 <65943793+nikhil2602@users.noreply.github.com>
2020-06-16 12:05:24 +0530

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call(String jobName,String buildNumber,String buildStatus,String stageName) {
-  echo "shared_library"
+  //echo "shared_library"
   //echo "Hello, ${name}."
   //slackSend (color: '#FFFF00', message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' SUCCESS")
   slackSend (color: '#FFFF00', message: "${stageName}: Job '${jobName} ${[buildNumber]}' ${buildStatus}")
